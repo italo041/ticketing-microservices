1131fda5fd3d7a83c55cf1112cf58ec0e90c22f4
italo041 <italo_fernando11@hotmail.com>
2022-08-23 19:54:48 -0500

    :sparles: Add tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,12 +1,17 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import request from 'supertest';
 
-let mongo: any;
+declare global {
+    var signin: () => Promise<string[]>;
+}
+
+let mongo: MongoMemoryServer;
 
 beforeAll(async () => {
     process.env.JWT_KEY = 'TEST12345'
-    const mongo = await MongoMemoryServer.create();
+    mongo = await MongoMemoryServer.create();
     const mongoUri =  mongo.getUri();
     
     await mongoose.connect(mongoUri, {});
@@ -24,7 +29,23 @@ beforeEach(async () => {
 
 afterAll(async () => {
     if (mongo) {
-        await mongoose.connection.close();
         await mongo.stop();
     }
+    mongoose.connection.close();
 })
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email, password
+        })
+        .expect(201)
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+}
