823fdbd46b72e9b7fd3228c83f8783a659e19449
italo041 <italo_fernando11@hotmail.com>
2022-09-07 22:28:52 -0500

    implement tdd

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError } from "@italo-tickets/common";
-
+import { createTicketRouter } from "./routes/new";
+createTicketRouter
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cookieSession({
   signed:false,
   secure: process.env.NODE_ENV !== 'test'
 }));
+
+app.use(createTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
